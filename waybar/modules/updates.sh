0a4b54f7dd0ee70d49098ca6d85dbd2595f90fb0
Aellas <zac1468@gmail.com>
2024-04-02 21:52:59 +0100

    fixed vivaldi not tiling

--- waybar/modules/updates.sh
+++ waybar/modules/updates.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-# Run the command to get the number of updates available
-updates_count=$(pacman -Qu | wc -l)
-# Format the result in a JSON-like format
-result="{\"updates_count\": $updates_count}"
+# Function to check for updates and update the Waybar display
+update_waybar() {
+    updates_count=$(pacman -Qu | wc -l)
+    echo "{\"text\":\" $updates_count    \"  ,\"tooltip\":\"$updates_count updates available\",\"class\":\"$updates_count\"}"
+}
 
-# Print the result
-echo "            $updates_count    "
+# Update Waybar display initially
+update_waybar
+
+# Continuously update Waybar display every 300 seconds (5 minutes)
+while true; do
+    sleep 300
+    update_waybar
+done
